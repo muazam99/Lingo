df19657fbab6a962e7fa742de2490e4bf95c6abf
Muaz Amir Nur Rashid <muazamir99@gmail.com>
2024-07-18 23:30:41 +0800

    update feed and sticky wrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,16 @@
+import { FeedWrapper } from "@/components/feed-wrapper"
+import { StickyWrapper } from "@/components/sticky-wrapper"
 
 const LearnPage = () => {
   return (
-    <div>LearnPage</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+        <StickyWrapper>
+            My sticky sidebar
+        </StickyWrapper>
+        <FeedWrapper>
+            My feed
+        </FeedWrapper>
+    </div>
   )
 }
 
