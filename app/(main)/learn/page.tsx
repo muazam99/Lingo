27f09e791703ae0d961f5c4aa27366d4f51cdc3d
Muaz Amir Nur Rashid <56258547+muazam99@users.noreply.github.com>
2024-07-20 12:26:47 +0800

    header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,14 +1,21 @@
 import { FeedWrapper } from "@/components/feed-wrapper"
 import { StickyWrapper } from "@/components/sticky-wrapper"
+import { Header } from "./header"
+import { UserProgress } from "@/components/user-progress"
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
         <StickyWrapper>
-            My sticky sidebar
+            <UserProgress
+              activeCourse ={{ title: "Spanish", imageSrc:"/es.svg" }}
+              hearts={5}
+              points={100}
+              hasActiveSubscription={false}
+           />
         </StickyWrapper>
         <FeedWrapper>
-            My feed
+            <Header title="Spanish"  /> 
         </FeedWrapper>
     </div>
   )
